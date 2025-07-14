e859cca00a22477202c1b0269066fdc875dda2e7
asim-08 <asimkhanwc786@gmail.com>
2025-07-14 23:48:20 +0500

    Finding some of first N natural number using loops

--- 07-Problem7.py
+++ 07-Problem7.py
@@ -1,3 +1,16 @@
+# Using While loop.
 n = int(input("Enter a number: "))
-for i in range(1,n+1):
-    print(f"The sum of {n} is {n*(n+1)/2}")
+i = 1
+sum = 0
+while i <= n:
+    sum += i
+    i += 1
+print(f"The sum of {n} natural number is: {sum}")
+
+# Using For loop.
+n = int(input("Enter a number: "))
+sum = 0
+for i in range (1,n+1):
+    sum += i
+print(f"The sum of {n} natural number is: {sum}")
+
