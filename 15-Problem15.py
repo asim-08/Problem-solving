1e2e29de35430d298b3f2aa03c9dc1c2b67a0e7a
asim-08 <asimkhanwc786@gmail.com>
2025-07-15 14:11:55 +0500

    Storing and printing the list of subjects

---
+++ 15-Problem15.py
@@ -0,0 +1,16 @@
+# Storing and printing the list of subjects.
+
+l = []
+
+a1 = input("Enter you'r subject1 : ")
+l.append(a1)
+a2 = input("Enter you'r subject2 : ")
+l.append(a2)
+a3 = input("Enter you'r subject3 : ")
+l.append(a3)
+a4 = input("Enter you'r subject4 : ")
+l.append(a4)
+a5 = input("Enter you'r subject5 : ")
+l.append(a5)
+
+print(l)
