bf7df291244865652a523b623f0aa876934b9934
asim-08 <asimkhanwc786@gmail.com>
2025-07-15 16:23:43 +0500

    Calculating the simple intrest

---
+++ 21-Problem21.py
@@ -0,0 +1,6 @@
+# Calculating the simple intrest.
+
+rs = 50000
+In = 5   # per year
+return_rs = 3          # in 3 year
+print(f"The toatal amount you will return with intrest is {(rs*In*return_rs)/100+50000}")
