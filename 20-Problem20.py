dffcf49b90b2ca2ed0ca40a1d3c3e29295946684
asim-08 <asimkhanwc786@gmail.com>
2025-07-15 15:53:08 +0500

    Finding the area of rectangle

---
+++ 20-Problem20.py
@@ -0,0 +1,6 @@
+# Finding the area of rectangle.
+
+L = int(input("Enter the lenght of rectangle here : "))
+W = int(input("Enter the width of rectangle here : "))
+
+print(f"The area of rectangle is: {L*W}")
