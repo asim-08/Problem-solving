d13a9681b9a9d0a7e4f84012474e005c247ec6b2
asim-08 <asimkhanwc786@gmail.com>
2025-07-15 14:27:06 +0500

    Declearing multiple variables in one line

---
+++ 16-problem16.py
@@ -0,0 +1,7 @@
+# Declearing multiple variable in one line
+
+x,y,z = 10, 20, "Asim"      # There are multiple ways and methods to declear multiple variable in one line in python.
+print(x,y,z)
+print(x)
+print(y)
+print(z)
