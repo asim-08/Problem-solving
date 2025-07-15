63bedd3e0026e4cc05cf590bcf2ddf464ff6a242
asim-08 <asimkhanwc786@gmail.com>
2025-07-15 15:29:11 +0500

    Using arithmetic operators on two numbers

---
+++ 18-Problem18.py
@@ -0,0 +1,19 @@
+# Using arithmetic operators on two numbers.
+
+a = 12
+b = 3
+
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a // b
+i = a % b
+x = a ** b
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(i)
+print(x)
