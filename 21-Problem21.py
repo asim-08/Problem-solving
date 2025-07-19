71776c7d701239874502dd4d48c4b0d169b2edb6
asim-08 <asimkhanwc786@gmail.com>
2025-07-19 22:56:51 +0500

    Calculating the simple intrest

--- 21-Problem21.py
+++ 21-Problem21.py
@@ -3,4 +3,5 @@
 rs = int(input("Enter a amount : "))
 In = 5   # per year
 return_rs = 3          # in 3 year
-print(f"The toatal amount you will return with intrest is {(rs*In*return_rs)/100 + rs}")
+x = (rs*In*return_rs)/100 + rs
+print(f"The toatal amount you will return with intrest is {x}")
