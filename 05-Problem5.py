44beeb40c163ff0a7600b498ac3e886d29e2810f
asim-08 <asimkhanwc786@gmail.com>
2025-07-20 02:06:15 +0500

    Making small calculator with Exception handling

--- 05-Problem5.py
+++ 05-Problem5.py
@@ -1,27 +1,29 @@
 # Making small calculator.
+try:
+    a = float(input("Enter first number: "))
+    print()
+    print("Select Operations")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (X)")
+    print("4. Division (/)")
 
-a = float(input("Enter first number: "))
-print()
-print("Select Operations")
-print("1. Addition (+)")
-print("2. Subtraction (-)")
-print("3. Multiplication (X)")
-print("4. Division (/)")
+    c = int(input("Enter you'r choice (1/2/3/4): "))
+    print()
+    b = float(input("Enter second number: "))
 
-c = int(input("Enter you'r choice (1/2/3/4): "))
-print()
-b = float(input("Enter second number: "))
-
-if (c == 1):
-    print("The sum of two number is: ", a + b)
-elif (c == 2):
-    print("The subtraction of two number is: ", a - b)
-elif (c == 3):
-    print("The multiplication of two number is: ", a * b)
-elif (c == 4):
-    if (b != 0):
-        print("The divison of two number is: ", a / b)
+    if (c == 1):
+        print("The sum of two number is: ", a + b)
+    elif (c == 2):
+        print("The subtraction of two number is: ", a - b)
+    elif (c == 3):
+        print("The multiplication of two number is: ", a * b)
+    elif (c == 4):
+        if (b != 0):
+            print("The divison of two number is: ", a / b)
+        else:
+            print("Error: Divided by 0")
     else:
-        print("Error: Divided by 0")
-else:
-    print("Invalid Input")
+        print("Invalid Input")
+except:
+    print("Enter a valid number")
