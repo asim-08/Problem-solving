f0bb7a55ea827cc6c96988636e6a8b9471768f13
asim-08 <asimkhanwc786@gmail.com>
2025-07-20 00:43:44 +0500

    Printing the fabonacci sequence

--- 26-Problem26.py
+++ 26-Problem26.py
@@ -1,5 +1,10 @@
+# Printing the fabonacci sequence.
 n = int(input("Enter a number : "))
-add = 1
-for i in range (0, n-1):
-    add = add + i
-    print(f"The fabonacci sum of {n} is {add}")
+a = 0
+b = 1
+
+for i in range(n):
+    print(a, end=" ")
+    c = a + b
+    a = b
+    b = c
