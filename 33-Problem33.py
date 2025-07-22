0998aa2a0161df7f18f1389a7897deccd76ed66d
asim-08 <asimkhanwc786@gmail.com>
2025-07-22 14:27:24 +0500

    Grading students based on marks

--- 33-Problem33.py
+++ 33-Problem33.py
@@ -2,15 +2,15 @@
 
 try:
     student_marks = int(input("Enter a student marks : "))
-    if student_marks >= 90 or student_marks <= 100:
+    if student_marks >= 90 and student_marks <= 100:
         print("Grade A+")
-    elif student_marks >= 80 or student_marks < 90:
+    elif student_marks >= 80 and student_marks < 90:
         print("Grade A")
-    elif student_marks >= 70 or student_marks < 80:
+    elif student_marks >= 70 and student_marks < 80:
         print("Grade B")
-    elif student_marks >= 60 or student_marks < 70:
+    elif student_marks >= 60 and student_marks < 70:
         print("Grade C")
-    elif student_marks >= 50 or student_marks < 60:
+    elif student_marks >= 50 and student_marks < 60:
         print("Grade D")
     elif student_marks > 100:
         print("Enter marks in range of 0 to 100")
