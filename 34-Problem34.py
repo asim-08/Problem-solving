9e3bbd826e9ea3d9f28a0ba78b29df574f716207
asim-08 <asimkhanwc786@gmail.com>
2025-07-22 19:07:09 +0500

    Leap year checker

--- 34-Problem34.py
+++ 34-Problem34.py
@@ -1,2 +1,15 @@
 # Leap year checker
-
+try:
+    year = int(input("Enter a year : "))
+    if (year <= 0):
+        print("Invalid year")
+    elif (year % 400 == 0):
+        print(f"{year} is a leap year")
+    elif (year % 100 == 0):
+        print(f"{year} is not a leap year")
+    elif (year % 4 == 0):
+        print(f"{year} is a leap year")
+    else:
+        print(f"{year} is not a leap year")
+except ValueError:
+    print("Enter a valid year")
