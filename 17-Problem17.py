61827a4caccf72c912d247772d12c4e683bcb419
asim-08 <asimkhanwc786@gmail.com>
2025-07-24 18:38:17 +0500

    Checking if number is dividible by 5 and 11"

--- 17-Problem17.py
+++ 17-Problem17.py
@@ -1,9 +1,11 @@
 # Checking if number is dividible by 5 and 11.
 
 n = int(input("Enter a number : "))
-if(n%11 == 0):
-    print(f"{n} is divisible by 11")
+if (n%5 == 0 and n%11 == 0):
+    print(f"{n} is divisible by both number 5 and 11")
 elif(n%5 == 0):
     print(f"{n} is divisible by 5")
+elif(n%11 == 0):
+    print(f"{n} is divisible by 11")
 else:
     print(f"{n} is not divisible by both number 5 and 11")
